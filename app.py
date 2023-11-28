8848ef96f6754582a9d524a9a7fc4f4bc6eec06e
Njikem <123434988+Njikem@users.noreply.github.com>
2023-11-28 16:32:23 -0700

    Function

---
+++ app.py
@@ -0,0 +1,25 @@
+# Function
+
+def names(name, food):
+    print(f"Hello {name}. Let eat some {food}")
+
+names('Alex', 'Pizza')
+
+
+def name(name, food='Tacos'):
+    print(f"hello {name}. let eat some {food}")
+
+
+name("Kalob")
+
+# To overwrite the value pizza you do
+name('John', 'oranges')
+
+#Exponent Calculator
+def exp (num1, num2): 
+    total = num1 * num2
+    return total
+
+big_numb = exp(33, 6)
+print(big_numb)
+
